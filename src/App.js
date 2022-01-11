2dc3427112d59193b6d1e16ae6f6c121c3ec44c2
Tommaso Ciarlariello <tomasciarlariello@gmail.com>
2022-01-11 08:05:38 -0500

    Add login functionality to allow simultaneous users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
+import Login from "./components/Login";
 
 // ChatEngine settings
 const App = () => {
+  // If user is not logged in, redirect to login page
+  if (!localStorage.getItem("username")) return <Login />;
+
   return (
     <ChatEngine
       height="100vh"
       projectID="099d4ba3-0f22-4091-ace2-9b9f747d7adb"
-      userName="master"
-      userSecret="sillygoose"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
     />
   );
 };
---
+++ src/components/Login.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import axios from "axios";
+
+// Login function for multiple simultaneous users
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Authenticate a user after login form submission
+    const authObject = {
+      "Project-ID": "099d4ba3-0f22-4091-ace2-9b9f747d7adb",
+      "User-Name": username,
+      "User-Secret": password
+    };
+
+    try {
+      // Login on success
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject
+      });
+
+      // Save user settings in the browser
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+
+      window.location.reload();
+    } catch (error) {
+      // Error message if credentials don't match current user
+      setError("Invalid credentials");
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Comms</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+            placeholder="Enter username"
+            required
+          />
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="Enter password"
+            required
+          />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Login</span>
+            </button>
+          </div>
+          <h2 className="error">{error}</h2>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

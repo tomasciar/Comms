8dc9fe485460e6c1becc5d39284c5d7cb962b27f
Tommaso Ciarlariello <tomasciarlariello@gmail.com>
2022-01-11 06:44:08 -0500

    Install dependencies; Basic functionality of application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
 
-function App() {
+// ChatEngine settings
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="099d4ba3-0f22-4091-ace2-9b9f747d7adb"
+      userName="master"
+      userSecret="sillygoose"
+    />
   );
-}
+};
 
 export default App;

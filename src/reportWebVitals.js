f8dc99dbe18a5b780bba6ffe0ac1616bf0eab8a8
Tommaso Ciarlariello <tomasciarlariello@gmail.com>
2022-01-12 03:01:08 -0500

    Update CSS; Add comments for future updated

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,6 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -10,4 +10,8 @@ const reportWebVitals = onPerfEntry => {
   }
 };
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Export for use in index.js
 export default reportWebVitals;
